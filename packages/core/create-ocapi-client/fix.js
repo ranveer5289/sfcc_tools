4e6007781726808803ff613db881ce1dba316068
ranveer raghuwanshi <>
2020-07-25 11:35:56 +0200

    minor refactoring

--- packages/core/create-ocapi-client/fix.js
+++ packages/core/create-ocapi-client/fix.js
@@ -2,18 +2,22 @@ const path = require('path');
 const fs = require('fs');
 
 function fixPackageJSON(apiName) {
-    const dataApiFolder = path.join(__dirname, '..', 'ocapi', apiName);
-    const dataApiPackageJSONFile = path.join(dataApiFolder, 'package.json');
+    const ocapiFolder = path.join(__dirname, '..', 'ocapi', apiName);
+    const ocapiJsonFile = path.join(ocapiFolder, 'package.json');
 
-    console.log(`reading file ${dataApiPackageJSONFile}`);
-    const packageJSONData = fs.readFileSync(dataApiPackageJSONFile);
+    console.log(`reading file ${ocapiJsonFile}`);
+    const packageJSONData = fs.readFileSync(ocapiJsonFile);
     const jsonObj = JSON.parse(packageJSONData.toString());
 
+    // modify below vars for lerna to work properly
     jsonObj.version = '1.0.0';
-    jsonObj.name = '@sfcc_tools/' + jsonObj.name;
+    if (jsonObj.name.indexOf('@sfcc_tools/') === -1) {
+        jsonObj.name = '@sfcc_tools/' + jsonObj.name;
+    }
+    jsonObj.private = true;
     console.log('fixing package.json');
 
-    fs.writeFileSync(dataApiPackageJSONFile, JSON.stringify(jsonObj, null, '\t'));
+    fs.writeFileSync(ocapiJsonFile, JSON.stringify(jsonObj, null, '\t'));
 }
 
 fixPackageJSON('shop-api');
