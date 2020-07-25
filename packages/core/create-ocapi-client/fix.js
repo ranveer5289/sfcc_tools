6e6a529810ce05120471308da80611a5ffa1a02a
ranveer raghuwanshi <>
2020-07-26 00:51:49 +0200

    added eslint

--- packages/core/create-ocapi-client/fix.js
+++ packages/core/create-ocapi-client/fix.js
@@ -1,21 +1,22 @@
 const path = require('path');
 const fs = require('fs');
+const chalk = require('chalk');
 
 function fixPackageJSON(apiName) {
     const ocapiFolder = path.join(__dirname, '..', 'ocapi', apiName);
     const ocapiJsonFile = path.join(ocapiFolder, 'package.json');
 
-    console.log(`reading file ${ocapiJsonFile}`);
+    console.log(chalk.green(`reading file ${ocapiJsonFile}`));
     const packageJSONData = fs.readFileSync(ocapiJsonFile);
     const jsonObj = JSON.parse(packageJSONData.toString());
 
     // modify below vars for lerna to work properly
     jsonObj.version = '1.0.0';
     if (jsonObj.name.indexOf('@sfcc_tools') === -1) {
-        jsonObj.name = '@sfcc_tools/' + jsonObj.name;
+        jsonObj.name = `@sfcc_tools/${jsonObj.name}`;
     }
     jsonObj.private = true;
-    console.log('fixing package.json');
+    console.log(chalk.green('fixing package.json'));
 
     fs.writeFileSync(ocapiJsonFile, JSON.stringify(jsonObj, null, '\t'));
 }
